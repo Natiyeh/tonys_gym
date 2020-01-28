49cd8f9398e3034ab700608329790990d4eddb8d
Nasser Atiyeh <nasseratiyeh@gmail.com>
2020-01-28 11:40:28 -0600

    created products table

---
+++ db/migrate/20200128173720_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.integer :merchant_id
+      t.float :price
+      t.integer :product_status
+
+      t.timestamps
+    end
+  end
+end
