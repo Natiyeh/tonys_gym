46bd508c6bd7772c3f02db11736a8fbec08bc3df
Nasser Atiyeh <nasseratiyeh@gmail.com>
2020-01-29 11:42:19 -0600

    added validations for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,10 @@
 class Course < ApplicationRecord
   belongs_to :intructor
+
+  has_many :course_events
+
+  validates :name, presence: true
+  validates :frequency, presence: true
+  validates :day_of_week, presence: true
+  validates :time_of_day, presence: true
 end
