0b581078c8a2be3a1fb08bc6ae6903f4b82fd0fc
Jasim Atiyeh <jasimatiyeh@gmail.com>
2020-01-29 11:37:48 -0600

    associations for course_event

--- app/models/course_event.rb
+++ app/models/course_event.rb
@@ -1,3 +1,5 @@
 class CourseEvent < ApplicationRecord
+  validates :scheduled_at, presence: :true
+  
   belongs_to :course
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, :merchant_id, :price, :product_status, presence: :true
+
   enum product_status: [:out_of_stock, :in_stock, :running_low]
 end
