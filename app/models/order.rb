2d796d44f88a4c2ee61ca3abf18e83e1ed51704f
Nasser Atiyeh <nasseratiyeh@gmail.com>
2020-01-29 11:38:34 -0600

    added validations for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  has_many :order_items
+  has_many :user_memberships
+
+  has_one :user_id
+
+  validates :status, presence: true
+  validates :created_at, presence: true
 end
