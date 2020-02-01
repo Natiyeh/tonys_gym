4d5e2ae4abd923e11bfd52268c16e17496bec941
Nasser Atiyeh <nasseratiyeh@gmail.com>
2020-02-01 10:32:17 -0600

    added routes for courses

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :course_events
   resources :course_participants
-
+  resources :courses
 
 end
